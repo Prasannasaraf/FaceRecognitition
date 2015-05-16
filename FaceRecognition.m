d25c0dc3194fdd59ce0d1aaff3ee9ae744dc2a16
Prasannasaraf <prasannasaraf180@gmail.com>
2015-05-16 15:51:42 +0530

    changed handles to guidata(hObject)

--- FaceRecognition.m
+++ FaceRecognition.m
@@ -61,7 +61,7 @@ function TrainDatabase_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 TrainDataBase(hObject,handles);
-checkiftrained(handles);
+checkiftrained(guidata(hObject));
 
 
 % --- Executes on button press in LoadImage.
